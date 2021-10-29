47f57e33fdde39863e0321d40c6da0f76be97049
K.Rahul Reddy <89081570+Rahulreddie@users.noreply.github.com>
2021-10-29 16:52:25 +0530

    swap numbers

---
+++ swap.java
@@ -0,0 +1,13 @@
+class  swap
+{
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		System.out.println("Numbers before swapping"+" a is :" +a+" b is :"+b);
+		int temp=a;
+		a=b;
+		b=temp;
+		System.out.println("Numbers after swapping"+" a is : "+a+" b is :"+b);
+	}
+}
